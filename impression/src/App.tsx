1a9a74cfa3c79021a5b7ee7e648139de8db85db5
ZI-DESKTOP <zc3huang@uwaterloo.ca>
2023-03-22 19:50:52 -0400

    added start page

--- impression/src/App.tsx
+++ impression/src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Start from './components/Start'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 function App() {
   return (
     <div className="App">
+      <Start/>
     </div>
   );
 }
---
+++ impression/src/components/Start/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './index.css';
+import Button from '@mui/material/Button';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Container from '@mui/material/Container';
+
+function Start() {
+    return (
+        <div className="Start">
+            <Button variant="text">Start</Button>
+        </div>
+    );
+}
+
+export default Start;
